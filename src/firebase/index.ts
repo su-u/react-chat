99fb9ba4d5f0dc586b4c0f736944357b9e709bbb
すー <aaaiiiuuu35@gmail.com>
2019-08-19 22:15:59 +0900

    Add: firebase/index

---
+++ src/firebase/index.ts
@@ -0,0 +1,6 @@
+// @ts-ignore
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseDb = firebaseApp.database();
