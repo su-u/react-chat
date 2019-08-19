0807a3d267b00b3f288dd9627f01db32da687e5f
すー <aaaiiiuuu35@gmail.com>
2019-08-20 00:32:22 +0900

    Add: message acions

--- src/actions/app.ts
+++ src/actions/app.ts
@@ -4,3 +4,18 @@ export function login(name: string) {
     login_user_name: name
   };
 }
+
+export function SendMessage(text: string, date: string) {
+  return {
+    type: "SEND_MESSAGE",
+    send_message: text,
+    send_date: date
+  };
+}
+
+export function ReceiveMessage(messageList: {[data: string]: string}) {
+  return {
+    type: "RECEIVE_MESSAGE",
+    message_list: messageList,
+  };
+}
