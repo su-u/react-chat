37e0eeb63a9c77e8ef1a898a0f1fa91aea229400
すー <aaaiiiuuu35@gmail.com>
2019-08-20 00:31:16 +0900

    Update: indent

--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -1,10 +1,10 @@
-import { Map } from "immutable";
+import {Map} from "immutable";
 
-export default function(state, action) {
-  switch (action.type) {
-    case "APP_LOGIN":
-      return state.set("login_user_name", action.login_user_name);
-    default:
-  }
-  return state || Map({ login_user_name: "" });
+export default function (state, action) {
+    switch (action.type) {
+        case "APP_LOGIN":
+            return state.set("login_user_name", action.login_user_name);
+        default:
+    }
+    return state || Map({login_user_name: ""});
 }
