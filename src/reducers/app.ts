901c41258d6d9cf60eb90ee5aa985030e651e80c
すー <aaaiiiuuu35@gmail.com>
2019-08-20 17:40:08 +0900

    Remove: send message action

--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -5,11 +5,6 @@ export default function (state, action) {
     switch (action.type) {
         case type.APP_LOGIN:
             return state.set("login_user_name", action.login_user_name);
-        case type.SEND_MESSAGE:
-            return state
-                .set("send_date", new Date().toString())
-                .set("send_name", action.send_date)
-                .set("send_message", action.send_message);
         case type.RECEIVE_MESSAGE:
             return state.set("message_list", action.message_list);
         default:
