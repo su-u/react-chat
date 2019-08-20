06963afc748f3814f2009463552548b79f8e012f
すー <aaaiiiuuu35@gmail.com>
2019-08-20 15:01:33 +0900

    Add: actions

--- src/actions/app.ts
+++ src/actions/app.ts
@@ -1,21 +1,24 @@
+import {Actions as type } from "../actions"
+
 export function login(name: string) {
   return {
-    type: "APP_LOGIN",
+    type: type.APP_LOGIN,
     login_user_name: name
   };
 }
 
-export function SendMessage(text: string, date: string) {
+export function SendMessage(text: string, name: string, date: string) {
   return {
-    type: "SEND_MESSAGE",
-    send_message: text,
-    send_date: date
+    type: type.SEND_MESSAGE,
+    send_date: date,
+    send_name: name,
+    send_message: text
   };
 }
 
 export function ReceiveMessage(messageList: {[data: string]: string}) {
   return {
-    type: "RECEIVE_MESSAGE",
+    type: type.RECEIVE_MESSAGE,
     message_list: messageList,
   };
 }
--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -1,9 +1,17 @@
 import {Map} from "immutable";
+import {Actions as type} from "../actions";
 
 export default function (state, action) {
     switch (action.type) {
-        case "APP_LOGIN":
+        case type.APP_LOGIN:
             return state.set("login_user_name", action.login_user_name);
+        case type.SEND_MESSAGE:
+            return state
+                .set("send_date", new Date().toString())
+                .set("send_name", action.send_date)
+                .set("send_message", action.send_message);
+        case type.RECEIVE_MESSAGE:
+            return state.set("message_list", action.message_list);
         default:
     }
     return state || Map({login_user_name: ""});
